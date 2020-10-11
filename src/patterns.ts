df73484ec3a1c24b35e7a0801ba26855e0f0f6c8
Marco <serpen@hotmail.de>
2020-10-11 16:03:28 +0200

    Pattern for doc comment with prefix space

--- src/patterns.ts
+++ src/patterns.ts
@@ -1,24 +1,24 @@
 /**
  * Matches a Function, 1st = Comment, 2nd = Definition, 3rd = Function/Sub, 4th = ?, 5th = Name, 6th = params
  */
-export const FUNCTION = /((?:^[\t ]*'+.*$(?:\r\n|\n))*)*^[\t ]*((?:(?:Public|Private)[\t ]+)?(Function|Sub)[\t ]+(([a-z]\w*)[\t ]*(?:\((.*)\))?))/img;
+export const FUNCTION = /((?:^[\t ]*'+.*$(?:\r\n|\n))*)^[\t ]*((?:(?:Public|Private)[\t ]+)?(Function|Sub)[\t ]+(([a-z]\w*)[\t ]*(?:\((.*)\))?))/img;
 
 /**
  * Matches a Class, 1st = Comment, 2nd = Name
  */
-export const CLASS    = /((?:^[\t ]*'+.*$(?:\r\n|\n))*)*^[\t ]*((?:(?:Public|Private)[\t ]+)?Class[\t ]+([a-z]\w*))/img;
+export const CLASS    = /((?:^[\t ]*'+.*$(?:\r\n|\n))*)^[\t ]*((?:(?:Public|Private)[\t ]+)?Class[\t ]+([a-z]\w*))/img;
 
 /**
  * Matches a Property, 1st = Comment, 2nd = Defintion, 3rd = Get/Let/Set, 4th = Name, 5th = params
  */
-export const PROP     = /((?:^[\t ]*'+.*$(?:\r\n|\n))*)*^[\t ]*((?:Public[\t ]+(?:Default[\t ]+)?|Private[\t ]+)?Property[\t ]+(Get|Let|Set)[\t ]+([a-z]\w*))(?:\((.*)\))?/img;
+export const PROP     = /((?:^[\t ]*'+.*$(?:\r\n|\n))*)^[\t ]*((?:Public[\t ]+(?:Default[\t ]+)?|Private[\t ]+)?Property[\t ]+(Get|Let|Set)[\t ]+([a-z]\w*))(?:\((.*)\))?/img;
 
 export const VAR = /(?<!'\s*)(?:^|:)[\t ]*(Dim|Set|Const|Private[\t ]+Const|Public[\t ]+Const|Private|Public)[\t ]+(?!Sub|Function|Class|Property)([a-z0-9_]+(?:[\t ]*\([\t ]*\d*[\t ]*\))?(?:[\t ]*,[\t ]*[a-z0-9_]+(?:[\t ]*\([\t ]*\d*[\t ]*\))?)*)[\t ]*.*(?:$|:)/img;
 export const VAR_COMPLS = /^[\t ]*(Dim|Const|((Private|Public)[\t ]+)?(Function|Sub|Class|Property [GLT]et))[\t ]+\w+[^:]*$/i; //fix: should again after var name #22
 
 export function DEF(input: string, word: string) {
   return new RegExp(
-    `((?:^'.*$(?:\r\n|\n))*)^[^'\\n\\r]*(\\b(?:Class|Const|Dim|Function|Property[\\t ][GLS]et|Sub)\\b[\\t ]+[\\w,\\t ]*\\b${word}\\b(?:[\\t ]*\(.*\))?)(?<!.*\\bRem\\b.*)`
+    `((?:^[\\t ]*'.*$(?:\r\n|\n))*)^[^'\\n\\r]*(\\b(?:Class|Const|Dim|Function|Property[\\t ][GLS]et|Sub)\\b[\\t ]+[\\w,\\t ]*\\b${word}\\b(?:[\\t ]*\(.*\))?)(?<!.*\\bRem\\b.*)`
     , "im").exec(input);
 }
 
