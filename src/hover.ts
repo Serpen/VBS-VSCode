c7cc37fc297e53987e7f08563f23ec1902d66977
Marco <serpen@hotmail.de>
2020-10-16 07:28:42 +0200

    naiv, primitiv param hover #12

--- src/hover.ts
+++ src/hover.ts
@@ -1,4 +1,4 @@
-import { languages, Hover, TextDocument, Position } from 'vscode';
+import { languages, Hover, TextDocument, Position, Range } from 'vscode';
 import * as PATTERNS from './patterns';
 import { GlobalSourceImport, ObjectSourceImport, SourceImports } from './extension';
 
@@ -28,12 +28,15 @@ export default languages.registerHoverProvider({ scheme: 'file', language: 'vbs'
     for (const ExtraDocText of SourceImports)
       hoverresults.push(...GetHover(ExtraDocText, word, "[Import]"));
 
+    // hoverresult for param must be above
+    hoverresults.push(...GetParamHover(document.getText(new Range(new Position(0, 0), new Position(position.line+1, 0))), word));
+
     if (hoverresults.length > 0)
       return hoverresults[0];
   },
 });
 
-function GetHover(docText: string, lookup: string, scope: string) : Hover[] {
+function GetHover(docText: string, lookup: string, scope: string): Hover[] {
   const results: Hover[] = [];
   let matches = PATTERNS.DEF(docText, lookup);
   if (matches) {
@@ -59,4 +62,20 @@ function GetHover(docText: string, lookup: string, scope: string) : Hover[] {
       results.push(new Hover({ language: "vbs", value: matches[2] + " ' " + scope }));
   }
   return results;
+}
+
+function GetParamHover(text: string, lookup: string): Hover[] {
+  const hovers: Hover[] = [];
+
+  let matches: RegExpExecArray;
+  while (matches = PATTERNS.FUNCTION.exec(text))
+    matches[6]?.split(",").filter(p => p.trim() === lookup).forEach(param => {
+      hovers.push(new Hover({ language: "vbs", value: lookup + " ' [Parameter]" }));
+    });
+
+  // last result should be nearest hit
+  if (hovers.length > 0)
+    return [hovers[hovers.length - 1]];
+  else
+    return [];
 }
