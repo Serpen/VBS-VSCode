59ded17577c9db177676e4e416f866b7a5cc224c
Marco <serpen@hotmail.de>
2020-10-16 07:29:22 +0200

    naiv, primitiv param definition #12

--- src/definition.ts
+++ src/definition.ts
@@ -1,4 +1,4 @@
-import { languages, Location, TextDocument, Position, Uri } from 'vscode';
+import { languages, Location, TextDocument, Position, Uri, Range } from 'vscode';
 import { GlobalSourceImport, GlobalSourceImportFile, ObjectSourceImport, ObjectSourceImportFile, SourceImportFiles, SourceImports as SourceImports } from './extension';
 import * as PATTERNS from './patterns';
 
@@ -24,14 +24,17 @@ export default languages.registerDefinitionProvider({ scheme: 'file', language: 
     for (let index = 0; index < SourceImports.length; index++)
       posLoc.push(...findExtDef(SourceImports[index], lookup, Uri.file(SourceImportFiles[index])))
 
+    // def for param must be above
+    posLoc.push(...GetParamDef(document.getText(new Range(new Position(0, 0), new Position(position.line + 1, 0))), lookup, document.uri));
+
     if (posLoc.length > 0)
       return posLoc[0];
   }
 },
 );
 
-function findExtDef(docText: string, lookup: string, docuri: Uri) : Location[] {
-  const posloc : Location[] = [];
+function findExtDef(docText: string, lookup: string, docuri: Uri): Location[] {
+  const posloc: Location[] = [];
   let match = PATTERNS.DEF(docText, lookup);
   if (match) {
     const line = docText.slice(0, match.index).match(/\n/g).length;
@@ -44,4 +47,21 @@ function findExtDef(docText: string, lookup: string, docuri: Uri) : Location[] {
     posloc.push(new Location(docuri, new Position(line, 0)));
   }
   return posloc;
+}
+
+function GetParamDef(docText: string, lookup: string, thisUri: Uri): Location[] {
+  const locs: Location[] = [];
+
+  let matches: RegExpExecArray;
+  while (matches = PATTERNS.FUNCTION.exec(docText))
+    matches[6]?.split(",").filter(p => p.trim() === lookup).forEach(param => {
+      const line = docText.slice(0, matches.index).match(/\n/g).length;
+      locs.push(new Location(thisUri, new Position(line, 0)));
+    });
+
+  // last result should be nearest hit
+  if (locs.length > 0)
+    return [locs[locs.length - 1]];
+  else
+    return [];
 }
