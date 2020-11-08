ed4591e38ac7d17877ac18bd0a9033fdf4ba3055
Marco <serpen@hotmail.de>
2020-11-08 10:33:02 +0100

    revert to reenable old runScript command #48

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,11 @@
-import { ExtensionContext, workspace } from "vscode";
+import { commands, ExtensionContext, workspace } from "vscode";
 import hoverProvider from "./hover";
 import completionProvider from "./completion";
 import symbolsProvider from "./symbols";
 import signatureProvider from "./signature";
 import definitionProvider from "./definition";
 import colorProvider from "./colorprovider";
+import * as cmds from "./commands";
 import { IncludeFile, Includes } from "./Includes";
 import { basename } from "path";
 import launchProvider from "./Launcher";
@@ -37,4 +38,14 @@ export function activate(context: ExtensionContext): void {
     launchProvider.launchConfigProvider,
     launchProvider.inlineDebugAdapterFactory
   );
+
+  // Run Script Command
+  commands.registerCommand("vbs.runScript", () => {
+    cmds.runScript();
+  });
+
+  // Kill running script command
+  commands.registerCommand("vbs.killScript", () => {
+    cmds.killScript();
+  });
 }
